0b9e575b6a7ce54710f19a5213d99168ee0ebf85
Майоров Андрей Артемьевич <a.a.maiorov@samsmu.ru>
2022-04-22 15:38:50 +0400

    changes
    add full adaptive for "Profile" page

--- js/show-menu.js
+++ js/show-menu.js
@@ -1,6 +1,6 @@
-let menuBtn = document.querySelector('.show-btn');
-let menu = document.querySelector('.navigation');
-menuBtn.addEventListener('click', function(){
+let sideBarBtn = document.querySelector('.show-btn');
+let sideBar = document.querySelector('.navigation');
+sideBarBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('hide');
-	menu.classList.toggle('hide');
+	sideBar.classList.toggle('hide');
 })
