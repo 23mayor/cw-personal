c2f8cfef7dc99a7d24c57b62af875a46ad81d165
Майоров Андрей Артемьевич <a.a.maiorov@samsmu.ru>
2022-04-24 21:30:38 +0400

    add admin page

--- js/show-menu.js
+++ js/show-menu.js
@@ -1,6 +1,6 @@
 let sideBarBtn = document.querySelector('.show-btn');
 let sideBar = document.querySelector('.navigation');
 sideBarBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('hide');
+	sideBarBtn.classList.toggle('hide');
 	sideBar.classList.toggle('hide');
 })
